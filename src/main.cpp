e3c73ee45071252132ea9d7cbcec41569c5fba5d
papapleygames <papapleygames@gmail.com>
2024-10-22 19:10:15 +0530

    Updated main.cpp
    Updated the screen resolution

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({640u, 480u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
     while (window.isOpen())
